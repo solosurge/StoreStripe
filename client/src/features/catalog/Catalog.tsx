e49b2dc75def61102cae66762f56aafc86e7f4d6
solosurge <solosurge@yahoo.com>
2022-08-12 11:25:20 -0500

    End of section 3

---
+++ client/src/features/catalog/Catalog.tsx
@@ -0,0 +1,17 @@
+import { useState, useEffect } from "react";
+import { Product } from "../../app/layout/models/products";
+import ProductList from "./ProductList";
+
+export default function Catalog() {
+  const [products, setProducts] = useState<Product[]>([]);
+
+useEffect(() => {
+  fetch('http://localhost:5000/api/products').then(response => response.json()).then(data => setProducts(data))
+}, [])
+
+  return (
+    <>
+      <ProductList products={products} />
+    </>
+  );
+}
